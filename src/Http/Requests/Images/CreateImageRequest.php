51f9ae95b50a1a6424fb1df768a5338eafc4a2c3
Jon Erickson <jon@deschutesdesigngroup.com>
2024-11-19 16:28:45 -0800

    Allow attachment and image requests to upload files

--- src/Http/Requests/Images/CreateImageRequest.php
+++ src/Http/Requests/Images/CreateImageRequest.php
@@ -4,12 +4,39 @@ declare(strict_types=1);
 
 namespace Perscom\Http\Requests\Images;
 
-use Perscom\Http\Requests\AbstractCreateRequest;
+use Saloon\Contracts\Body\HasBody;
+use Saloon\Data\MultipartValue;
+use Saloon\Enums\Method;
+use Saloon\Http\Request;
+use Saloon\Traits\Body\HasMultipartBody;
 
-class CreateImageRequest extends AbstractCreateRequest
+class CreateImageRequest extends Request implements HasBody
 {
-    protected function getResource(): string
+    use HasMultipartBody;
+
+    protected Method $method = Method::POST;
+
+    /**
+     * @param  array<string, mixed>  $data
+     */
+    public function __construct(public array $data)
+    {
+        //
+    }
+
+    public function resolveEndpoint(): string
     {
         return 'images';
+    }
+
+    /**
+     * @return MultipartValue[]
+     */
+    protected function defaultBody(): array
+    {
+        return collect($this->data)->map(fn ($value, $key) => new MultipartValue(
+            name: $key,
+            value: $value
+        ))->toArray();
     }
 }
--- src/Http/Requests/Users/Attachments/CreateUserAttachmentRequest.php
+++ src/Http/Requests/Users/Attachments/CreateUserAttachmentRequest.php
@@ -4,12 +4,36 @@ declare(strict_types=1);
 
 namespace Perscom\Http\Requests\Users\Attachments;
 
-use Perscom\Http\Requests\AbstractRelationalCreateRequest;
+use Saloon\Contracts\Body\HasBody;
+use Saloon\Data\MultipartValue;
+use Saloon\Enums\Method;
+use Saloon\Http\Request;
+use Saloon\Traits\Body\HasMultipartBody;
 
-class CreateUserAttachmentRequest extends AbstractRelationalCreateRequest
+class CreateUserAttachmentRequest extends Request implements HasBody
 {
-    protected function getResource(int $relationId): string
+    use HasMultipartBody;
+
+    protected Method $method = Method::POST;
+
+    public function __construct(public int $relationId, public array $data)
     {
-        return "users/$relationId/attachments";
+        //
+    }
+
+    public function resolveEndpoint(): string
+    {
+        return "users/$this->relationId/attachments";
+    }
+
+    /**
+     * @return MultipartValue[]
+     */
+    protected function defaultBody(): array
+    {
+        return collect($this->data)->map(fn ($value, $key) => new MultipartValue(
+            name: $key,
+            value: $value
+        ))->toArray();
     }
 }
