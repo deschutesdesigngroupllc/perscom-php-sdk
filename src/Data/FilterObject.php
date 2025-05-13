bce6553ac38468b09cd4cea5bc634f6ffdc197ec
JannesD <jannesdrijkoningen@gmail.com>
2025-05-13 20:00:56 +0200

    fix type annotation in FilterObject
    Type annotation does not match type-hint, causing static analysis tools to provide bad feedback.

--- src/Data/FilterObject.php
+++ src/Data/FilterObject.php
@@ -11,7 +11,7 @@ final class FilterObject implements Arrayable
     /**
      * @param  string  $field  The field to perform the filter on.
      * @param  string  $operator  The supported operators are '<', '<=', '>', '>=', '=', '!=', 'like', 'not like', 'in', 'not in'.
-     * @param  string  $value  The value to filter by.
+     * @param  mixed  $value  The value to filter by.
      * @param  string  $type  The supported types are 'or' and 'and'. Defaults to OR.
      */
     public function __construct(
